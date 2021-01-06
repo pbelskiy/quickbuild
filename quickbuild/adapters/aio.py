155b23299eec310215c0f405788e94df3b268791
Petr Belskiy <petr.belskiy@gmail.com>
2021-01-06 15:02:48 +0300

    skeleton

---
+++ quickbuild/adapters/aio.py
@@ -0,0 +1,144 @@
+import asyncio
+
+from typing import Any, Callable, Optional, Union
+
+from aiohttp import (
+    BasicAuth,
+    ClientError,
+    ClientResponse,
+    ClientSession,
+    ClientTimeout,
+)
+
+from quickbuild.core import QuickBuild, QuickBuildError, Response
+
+
+class RetryClientSession:
+
+    def __init__(self, loop: Optional[asyncio.AbstractEventLoop], options: dict):
+        self.total = options.get('total') or 1
+        self.factor = options.get('factor', 0)
+        self.statuses = options.get('statuses', [])
+
+        self.session = ClientSession(loop=loop)
+
+    async def request(self, *args: Any, **kwargs: Any) -> ClientResponse:
+        for total in range(self.total):
+            try:
+                response = await self.session.request(*args, **kwargs)
+            except (ClientError, asyncio.TimeoutError) as e:
+                if total + 1 == self.total:
+                    raise QuickBuildError from e
+            else:
+                if response.status not in self.statuses:
+                    break
+
+            await asyncio.sleep(self.factor * (2 ** (total - 1)))
+
+        return response
+
+    async def close(self) -> None:
+        await self.session.close()
+
+
+class AsyncQBClient(QuickBuild):
+
+    session = None  # type: Union[ClientSession, RetryClientSession]
+    timeout = None
+
+    def __init__(self,
+                 url: str,
+                 user: str,
+                 password: str,
+                 *,
+                 loop: Optional[asyncio.AbstractEventLoop] = None,
+                 verify: bool = True,
+                 timeout: Optional[float] = None,
+                 retry: Optional[dict] = None
+                 ):
+        """
+        QuickBuild async client class.
+
+        * url: ``str``
+          Url of QuickBuild server, must include API version.
+
+        * user: ``str``
+          User name, login.
+
+        * password: ``str``
+          Password for user.
+
+        * loop: ``AbstractEventLoop`` (optional)
+          Asyncio current event loop.
+
+        * verify: ``bool`` (optional)
+          Verify SSL (default: true).
+
+        * timeout: ``int``, (optional)
+          HTTP request timeout.
+
+        * retry: ``dict`` (optional)
+          Retry options to prevent failures if server restarting or temporary
+          network problem.
+
+          - total: ``int`` Total retries count. (default 0)
+          - factor: ``int`` Sleep between retries (default 0)
+            {factor} * (2 ** ({number of total retries} - 1))
+          - statuses: ``List[int]`` HTTP statues retries on. (default [])
+
+          Example:
+
+          .. code-block:: python
+
+            retry = dict(
+                attempts=10,
+                factor=1,
+                statuses=[500]
+            )
+
+        :returns: ``AsyncClient instance``
+        :raises: ``QuickBuildError``
+        """
+        super().__init__()
+
+        self.loop = loop or asyncio.get_event_loop()
+        self.host = url
+
+        self.auth = BasicAuth(user, password)
+
+        if retry:
+            self.session = RetryClientSession(loop, retry)
+        else:
+            self.session = ClientSession(loop=self.loop)
+
+        self.verify = verify
+
+        if timeout:
+            self.timeout = ClientTimeout(total=timeout)
+
+    async def close(self) -> None:  # type: ignore
+        await self.session.close()
+
+    async def request(self,  # type: ignore
+                      callback: Callable[[Response], str],
+                      method: str,
+                      path: str,
+                      **kwargs: Any
+                      ) -> str:
+
+        if self.timeout and 'timeout' not in kwargs:
+            kwargs['timeout'] = self.timeout
+
+        response = await self.session.request(
+            method,
+            '{host}/rest/{path}'.format(
+                host=self.host,
+                path=path,
+            ),
+            auth=self.auth,
+            ssl=self.verify,
+            **kwargs
+        )
+
+        body = await response.text()
+        return callback(Response(response.status, body))
