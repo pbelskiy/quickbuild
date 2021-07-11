61d67361532a187f4de911aba51e19b6576f96a3
Petr Belskiy <petr.belskiy@gmail.com>
2021-07-11 14:44:00 +0300

    add identifiers.get_resource_id_by_name()

--- quickbuild/endpoints/identifiers.py
+++ quickbuild/endpoints/identifiers.py
@@ -1,10 +1,42 @@
+from typing import Any
+
 from quickbuild.helpers import response2py
 
 
 class Identifiers:
+    """
+    Most of QuickBuild RESTful API relies on identifier of the object,
+    which can be retrieved with the id service explained here.
 
+    https://wiki.pmease.com/display/QB10/Retrieve+Object+Identifier
+    """
     def __init__(self, quickbuild):
         self.quickbuild = quickbuild
+
+    def _get(self, params: dict) -> Any:
+        return self.quickbuild._request(
+            'GET',
+            'ids',
+            callback=response2py,
+            params=params,
+        )
+
+    def get_resource_id_by_name(self, name: str) -> int:
+        """
+        Get resource id by name.
+
+        **This feature is available since 6.1.35**
+
+        Args:
+            name (str): resource path.
+
+        Returns:
+            int: resource identifier.
+
+        Raises:
+            QBProcessingError: will be raised if resource is not found.
+        """
+        return self._get(dict(resource_name=name))
 
     def get_configuration_id_by_path(self, path: str) -> int:
         """
@@ -15,14 +47,8 @@ class Identifiers:
 
         Returns:
             int: configuration identifier.
-        """
-        params = dict(
-            configuration_path=path
-        )
 
-        return self.quickbuild._request(
-            'GET',
-            'ids',
-            callback=response2py,
-            params=params,
-        )
+        Raises:
+            QBProcessingError: will be raised if resource is not found.
+        """
+        return self._get(dict(configuration_path=path))
--- tests/test_identifiers.py
+++ tests/test_identifiers.py
@@ -4,6 +4,19 @@ import responses
 
 
 @responses.activate
+def test_get_resource_id_by_name(client):
+    responses.add(
+        responses.GET,
+        re.compile(r'.*/rest/ids'),
+        content_type='application/text',
+        body='1',
+    )
+
+    response = client.identifiers.get_resource_id_by_name('root')
+    assert response == 1
+
+
+@responses.activate
 def test_get_configuration_id_by_path(client):
     responses.add(
         responses.GET,
@@ -14,4 +27,3 @@ def test_get_configuration_id_by_path(client):
 
     response = client.configurations.get_id_by_path('root')
     assert response == 1
-
