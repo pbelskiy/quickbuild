c0121788eb1fe0ab90897cf28b25824cb3517f5e
Petr Belskiy <petr.belskiy@gmail.com>
2021-07-26 01:15:00 +0300

    add test for groups.get_id_by_name()

--- tests/test_identifiers.py
+++ tests/test_identifiers.py
@@ -56,6 +56,19 @@ def test_get_user_id_by_name(client):
 
 
 @responses.activate
+def test_get_group_id_by_name(client):
+    responses.add(
+        responses.GET,
+        re.compile(r'.*/rest/ids'),
+        content_type='application/text',
+        body='1',
+    )
+
+    response = client.groups.get_id_by_name('group')
+    assert response == 1
+
+
+@responses.activate
 def test_get_build_id_by_request_id(client):
     responses.add(
         responses.GET,
