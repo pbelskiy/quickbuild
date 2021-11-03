c2f8e2fb3991461e052e9b8c6e3f568e8348d807
Petr Belskiy <petr.belskiy@gmail.com>
2021-11-04 00:04:34 +0300

    add issues.get_size()

--- quickbuild/endpoints/issues.py
+++ quickbuild/endpoints/issues.py
@@ -1,3 +1,8 @@
+from typing import Optional
+
+from quickbuild.helpers import response2py
+
+
 class Tracker:
 
     def __init__(self, quickbuild, name: str):
@@ -14,6 +19,54 @@ class Tracker:
         return self.quickbuild._request(
             'GET',
             '{}/version'.format(self.name)
+        )
+
+    def get_size(self,
+                 configuration: str,
+                 *,
+                 build: Optional[int] = None,
+                 from_build: Optional[int] = None,
+                 to_build: Optional[int] = None
+                 ) -> int:
+        """
+        Retrieve the issues size related to a configuration.
+
+        Args:
+            configuration (str):
+                Specify the configuration. By default, specify configuration id here,
+                if you want to specify a configuration path, you need add prefix PATH:,
+                for example: `PATH:root/My/DEV`.
+
+                Since QB version 5.0.14: Specify the configuration path directly
+                for example: `root/My/DEV`, no need `PATH:prefix` anymore.
+
+            build (int):
+                The id of a specific build.
+
+            from_build (int):
+                Specify the from build when finding changes in a build range.
+
+            to_build (int):
+                Specify the to build when finding changes in a build range.
+
+        Returns:
+            int: issues size
+        """
+        params = dict()
+
+        if build:
+            params['build'] = build
+
+        if from_build:
+            params['from_build'] = from_build
+
+        if to_build:
+            params['to_build'] = to_build
+
+        return self.quickbuild._request(
+            'GET',
+            '{}/size/{}'.format(self.name, configuration),
+            callback=response2py,
         )
 
 
@@ -34,6 +87,6 @@ class Issues:
         - Bugzilla - /rest/bugzilla
 
         Returns:
-            List[dict]: list of resources.
+            List[dict]: list of resources
         """
         return Tracker(self.quickbuild, name)
--- tests/test_issues.py
+++ tests/test_issues.py
@@ -14,3 +14,21 @@ def test_get_version(client):
 
     version = client.issues.get_tracker('jira').get_version()
     assert version == '3.1'
+
+
+@responses.activate
+def test_get_size(client):
+    responses.add(
+        responses.GET,
+        re.compile(r'.*/rest/jira/size/.*'),
+        content_type='text/plain',
+        body='29',
+    )
+
+    size = client.issues.get_tracker('jira').get_size(
+        'root/My/DEV',
+        from_build=100,
+        to_build=150,
+    )
+
+    assert size == 29
