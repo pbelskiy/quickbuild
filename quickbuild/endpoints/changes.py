a01c9def6cb8a7e3413a6d91d57efcacff1b721e
Petr Belskiy <petr.belskiy@gmail.com>
2021-12-12 01:46:30 +0300

    add changes.get_commit_stats()

--- quickbuild/endpoints/changes.py
+++ quickbuild/endpoints/changes.py
@@ -1,3 +1,8 @@
+from typing import Any, Dict, List, Optional
+
+from quickbuild.helpers import response2py
+
+
 class Changes:
 
     def __init__(self, quickbuild):
@@ -11,3 +16,88 @@ class Changes:
             str: changes version.
         """
         return self.quickbuild._request('GET', 'changes/version')
+
+    def get_commit_stats(self,
+                         configuration: str,
+                         *,
+                         build: Optional[int] = None,
+                         from_build: Optional[int] = None,
+                         to_build: Optional[int] = None,
+                         from_date: Optional[str] = None,
+                         to_date: Optional[str] = None,
+                         date_pattern: Optional[str] = None,
+                         repository: Optional[str] = None,
+                         committer: Optional[str] = None
+                         ) -> dict:
+        """
+        Get the commit stats
+
+        Args:
+            configuration (str):
+                Specify the configuration. By default, specify configuration id
+                here, also, you can specify the configuration path directly.
+
+            build (Optional[int]):
+                Specify the build id you want.
+
+            from_build (Optional[int]):
+                Specify the from build when finding changes in a build range.
+
+            to_build (Optional[int]):
+                Specify the to build when finding changes in a build range.
+
+            from_date (Optional[str]):
+                Specify the from date when finding changes in a build range.
+
+            to_date (Optional[str]):
+                Specify the to date when finding changes in a build range.
+
+            date_pattern (Optional[str]):
+                Specify the date pattern when query by a date range, by default
+                the pattern is `yyyyMMdd`. Valid date pattern can be found here
+
+                https://docs.oracle.com/javase/1.5.0/docs/api/java/text/SimpleDateFormat.html
+
+            repository (Optional[str]):
+                Get the changes only in a specific repository.
+
+            committer (Optional[str]):
+                Get the changes only committed by a specified committer
+
+        Returns:
+            dict: commit stats
+        """
+        params = dict()  # type: Dict[str, Any]
+
+        if build:
+            params['build'] = build
+
+        if from_build:
+            params['from_build'] = from_build
+
+        if to_build:
+            params['to_build'] = to_build
+
+        if from_date:
+            params['from_date'] = from_date
+
+        if to_date:
+            params['to_date'] = to_date
+
+        if date_pattern:
+            params['date_pattern'] = date_pattern
+
+        if repository:
+            params['repository'] = repository
+
+        if committer:
+            params['committer'] = committer
+
+        response = self.quickbuild._request(
+            'GET',
+            'changes/stats/{}'.format(configuration),
+            callback=response2py,
+            params=params,
+        )
+
+        return response
--- tests/test_changes.py
+++ tests/test_changes.py
@@ -14,3 +14,31 @@ def test_get_version(client):
 
     version = client.changes.get_version()
     assert version == '3.2'
+
+
+@responses.activate
+def test_get_commit_stats(client):
+    responses.add(
+        responses.GET,
+        re.compile(r'.*/rest/changes/stats/.+'),
+        content_type='text/xml',
+        body='<stats commits="5" modifications="7" added="2" modified="3" deleted="2"/>',
+    )
+
+    stats = client.changes.get_commit_stats(
+        1,
+        build=9,
+        from_build=1,
+        to_build=333,
+        from_date='19911209',
+        to_date='20211209',
+        date_pattern='yyyyMMdd',
+        repository='main',
+        committer='p.belskiy',
+    )
+
+    assert stats['commits'] == 5
+    assert stats['modifications'] == 7
+    assert stats['added'] == 2
+    assert stats['modified'] == 3
+    assert stats['deleted'] == 2
