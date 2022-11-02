525fb262e5f8c8c9c935533e9ee87b6273a3b78d
Petr Belskiy <petr.belskiy@gmail.com>
2022-11-02 23:26:21 +0300

    fix pylint

--- quickbuild/exceptions.py
+++ quickbuild/exceptions.py
@@ -2,32 +2,27 @@ class QBError(Exception):
     """
     Core library exception
     """
-    ...
 
 
 class QBProcessingError(QBError):
     """
     Raises when request return HTTP code 204 (no content)
     """
-    ...
 
 
 class QBForbiddenError(QBError):
     """
     Raises when request return HTTP code 403 (forbidden)
     """
-    ...
 
 
 class QBNotFoundError(QBError):
     """
     Raises when request return HTTP code 404 (not found)
     """
-    ...
 
 
 class QBServerError(QBError):
     """
     Raises when request return HTTP code 500 (server error)
     """
-    ...
